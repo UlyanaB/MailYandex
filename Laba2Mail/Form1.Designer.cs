7e5cf41d72414db40462d926e0683c999dcceb39
Ульяна Борисовец <borisovets.u@mail.ru>
2021-01-09 21:17:54 +1000

    Добавьте файлы проекта.

---
+++ Laba2Mail/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Laba2Mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.TextBoxEmail = new System.Windows.Forms.TextBox();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.TextBoxTo = new System.Windows.Forms.TextBox();
+            this.Label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TextBoxTatle = new System.Windows.Forms.TextBox();
+            this.TextBoxText = new System.Windows.Forms.TextBox();
+            this.BtnFile = new System.Windows.Forms.Button();
+            this.BtnAvtor = new System.Windows.Forms.Button();
+            this.LableFile = new System.Windows.Forms.Label();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnAvtor);
+            this.groupBox1.Controls.Add(this.TextBoxPassword);
+            this.groupBox1.Controls.Add(this.TextBoxEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 17);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(354, 186);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Отправитель";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TextBoxTo);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(15, 232);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(354, 93);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Получатель";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Почта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(11, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Кому";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.LableFile);
+            this.groupBox3.Controls.Add(this.BtnFile);
+            this.groupBox3.Controls.Add(this.TextBoxText);
+            this.groupBox3.Controls.Add(this.TextBoxTatle);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.Label);
+            this.groupBox3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(412, 17);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Size = new System.Drawing.Size(456, 494);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Письмо";
+            // 
+            // TextBoxEmail
+            // 
+            this.TextBoxEmail.Location = new System.Drawing.Point(82, 29);
+            this.TextBoxEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxEmail.Name = "TextBoxEmail";
+            this.TextBoxEmail.Size = new System.Drawing.Size(258, 28);
+            this.TextBoxEmail.TabIndex = 2;
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Location = new System.Drawing.Point(82, 75);
+            this.TextBoxPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.Size = new System.Drawing.Size(258, 28);
+            this.TextBoxPassword.TabIndex = 3;
+            // 
+            // TextBoxTo
+            // 
+            this.TextBoxTo.Location = new System.Drawing.Point(82, 37);
+            this.TextBoxTo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxTo.Name = "TextBoxTo";
+            this.TextBoxTo.Size = new System.Drawing.Size(258, 28);
+            this.TextBoxTo.TabIndex = 4;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label.Location = new System.Drawing.Point(8, 34);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(47, 21);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "Тема";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(11, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 21);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Сообщение";
+            // 
+            // TextBoxTatle
+            // 
+            this.TextBoxTatle.Location = new System.Drawing.Point(74, 34);
+            this.TextBoxTatle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxTatle.Name = "TextBoxTatle";
+            this.TextBoxTatle.Size = new System.Drawing.Size(374, 28);
+            this.TextBoxTatle.TabIndex = 2;
+            // 
+            // TextBoxText
+            // 
+            this.TextBoxText.Location = new System.Drawing.Point(15, 156);
+            this.TextBoxText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxText.Multiline = true;
+            this.TextBoxText.Name = "TextBoxText";
+            this.TextBoxText.Size = new System.Drawing.Size(422, 151);
+            this.TextBoxText.TabIndex = 3;
+            // 
+            // BtnFile
+            // 
+            this.BtnFile.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnFile.Location = new System.Drawing.Point(15, 344);
+            this.BtnFile.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BtnFile.Name = "BtnFile";
+            this.BtnFile.Size = new System.Drawing.Size(423, 42);
+            this.BtnFile.TabIndex = 4;
+            this.BtnFile.Text = "Прекрепить файл";
+            this.BtnFile.UseVisualStyleBackColor = true;
+            this.BtnFile.Click += new System.EventHandler(this.BtnFile_Click);
+            // 
+            // BtnAvtor
+            // 
+            this.BtnAvtor.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnAvtor.Location = new System.Drawing.Point(15, 133);
+            this.BtnAvtor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BtnAvtor.Name = "BtnAvtor";
+            this.BtnAvtor.Size = new System.Drawing.Size(326, 46);
+            this.BtnAvtor.TabIndex = 4;
+            this.BtnAvtor.Text = "Авторизация";
+            this.BtnAvtor.UseVisualStyleBackColor = true;
+            this.BtnAvtor.Click += new System.EventHandler(this.BtnAvtor_Click);
+            // 
+            // LableFile
+            // 
+            this.LableFile.AutoSize = true;
+            this.LableFile.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LableFile.Location = new System.Drawing.Point(15, 428);
+            this.LableFile.Name = "LableFile";
+            this.LableFile.Size = new System.Drawing.Size(47, 21);
+            this.LableFile.TabIndex = 5;
+            this.LableFile.Text = "Файл";
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnSend.Location = new System.Drawing.Point(15, 471);
+            this.BtnSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(354, 39);
+            this.BtnSend.TabIndex = 3;
+            this.BtnSend.Text = "Отправить";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 526);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Mail";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnAvtor;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.TextBox TextBoxEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox TextBoxTo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label LableFile;
+        private System.Windows.Forms.Button BtnFile;
+        private System.Windows.Forms.TextBox TextBoxText;
+        private System.Windows.Forms.TextBox TextBoxTatle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button BtnSend;
+    }
+}
+
---
+++ Laba2Mail/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Laba2Mail
+{
+    public partial class Form1 : Form
+    {
+        SmtpClient smtp;
+        private string email = "";
+        private string password = "";
+       
+        private string file = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            TextBoxPassword.PasswordChar = '*';
+        }
+
+        public void Avtor()
+        {
+            try
+            {
+                smtp = new SmtpClient("smtp.yandex.ru", 25)
+                {
+                    Credentials = new NetworkCredential(email, password),
+                    EnableSsl = true
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnAvtor_Click(object sender, EventArgs e)
+        {
+            email = TextBoxEmail.Text;
+            password = TextBoxPassword.Text;
+
+            if(email == "" || password == "")
+            {
+                MessageBox.Show("Введите данные в поля почта и пароль");
+            }
+            else
+            {
+                Avtor();
+            }
+        }
+
+        private void BtnFile_Click(object sender, EventArgs e)
+        {
+           OpenFileDialog OFD = new OpenFileDialog();
+           OFD.Filter = "all| *.*";
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                file = OFD.FileName;
+                LableFile.Text = OFD.FileName;
+            }
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+           
+                try
+                {
+                    MailAddress from = new MailAddress(email);
+                    MailAddress to = new MailAddress(TextBoxTo.Text);
+                    MailMessage message = new MailMessage(from, to)
+                    {
+                        Subject = TextBoxTatle.Text,
+                        IsBodyHtml = false,
+                        Body = TextBoxText.Text,
+                    };
+
+                    if (file != "") message.Attachments.Add(new Attachment($"{file}"));
+                    smtp.Send(message);
+                    MessageBox.Show("Сообщение отправлено!");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Неверный формат электронной почты. Почта должна иметь окончания - ****@****.**");
+                    TextBoxTo.Clear();
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("Строка с адресом не должна быть пуста");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            
+        }
+    }
+}
